049393eefef5d212c41fc70fef55d412ef882d9d
DevalVagadiya <devalvagadiya2005@gmail.com>
2026-02-10 14:47:05 +0530

    Update SECRET_KEY to use environment variable for enhanced security

--- myproject/settings.py
+++ myproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h!z=qosj8=rz*b8#n2yze!&26f76m45xe5i(7si8s$4$be752o'
+SECRET_KEY = 'os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
