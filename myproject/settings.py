190047bd7d59c54ea26642a5a9a9b7cc68a54b53
DevalVagadiya <devalvagadiya2005@gmail.com>
2026-02-10 13:38:47 +0530

    Add dj_database_url import and update ALLOWED_HOSTS for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = [
     ".vercel.app",
     "localhost",
     "127.0.0.1",
+    'myapp.onrender.com'
 ]
 
 
